7233e5e2e130e4fd012be3109b24ee7014165c50
John <mrphysh@juno.com>
2019-01-09 12:11:01 -0500

    Update arduino_one_pendulum_good.ino

--- arduino_one_pendulum_good/arduino_one_pendulum_good.ino
+++ arduino_one_pendulum_good/arduino_one_pendulum_good.ino
@@ -1,4 +1,13 @@
+/* # The compiler never gets hung up with an 'if' statement.  It flies along and trips these triggers setting the times.  
+The data goes out through the serial monitor as a flat file; just a series of numbers.  The Python catches these numbers,
+sorts them by size and puts them accordingly in the sqlite3 database.
+I was using photo interrupter modules.  The pendulum had a little vane that interrupted the IR radiation.  
+It worked fine, but was delicate.  They defaulted LOW.  I moved to hall effect detectors.  These are magnetic.  
+So there is a little magnet on the pendulum.  They default HIGH.  I am assuming that I need to change 
+the HIGHS to LOWS and vice versa.  Then it occurred to me that it might work fine with no change. and that was the case.
+My LEDs instead of flashing on, flash off.  But the program is exactly the same.  I thought that was cool.
 
+*/
 
 
 #define led_out 9
